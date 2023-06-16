63d5f9bba9abed1f8f397eb80de1a942694f2a66
software-babooi <sekharchennaiah12345ctk@gmail.com>
2023-06-16 18:46:00 +0530

    making static folder for images

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.write("""
         This web application predicts the species type to which it belongs
     """)
 
-st.image("main.jpeg", use_column_width=True)
+st.image("static/main.jpeg", use_column_width=True)
 
 st.write("""
     To use this app, please fill out the following form and click on the 'Predict' button. You will then see the result.
@@ -38,11 +38,11 @@ final_pred=pred_pipe.predict(pred_df)
 if st.button('Predict'):
     if final_pred[0]==0:
         st.write('According to your information, the predicted specie is as follows:-')
-        st.image('iris-setosa.png',use_column_width=True)
+        st.image('static/iris-setosa.png',use_column_width=True)
     elif final_pred[0]==1:
         
         st.write('According to your information, the predicted species is Iris Versicolor')
-        st.image('iris-versicolor.jpeg',use_column_width=True)
+        st.image('static/iris-versicolor.jpeg',use_column_width=True)
     elif final_pred[0]==2:
         st.write('According to your information, the predicted species is Iris Virginica')
-        st.image('iris-virginica.png',use_column_width=True)
+        st.image('static/iris-virginica.png',use_column_width=True)
